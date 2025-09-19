dea2eaae2c3c9d4ca8a12dbca00d1764b44badd9
Murilo de Carvalho <mumucarvalho01@gmail.com>
2025-09-18 21:20:05 -0300

    Rewrote the all the code with Qiskit qubit ordering in mind

--- main.py
+++ main.py
@@ -1,268 +1,250 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit.library.standard_gates import XGate
 import numpy as np
 import matplotlib.pyplot as plt
 
-from qiskit import QuantumCircuit, transpile
-from qiskit.providers.fake_provider import GenericBackendV2
-from qiskit.visualization import plot_histogram
-
 from qiskit.quantum_info import Statevector
 
-def getDegree(node_index):
-    degree = 0
-    for i in range(0, mat_size):
-        if adj_matrix[node_index][i]:
-            degree += 1
+# ===================================
+# Global variables
+# ===================================
+
+num_nodes = 8
+adj_matrix = [
+    [0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0]
+]
+
+#num_nodes = 4
+#adj_matrix = [
+#    [0, 1, 1, 1],
+#    [1, 0, 1, 0],
+#    [1, 1, 0, 0],
+#    [1, 0, 0, 0]
+#]
+
+#num_nodes = 5
+#adj_matrix = [
+#    [0, 1, 0, 1, 0],
+#    [1, 0, 1, 0, 0],
+#    [0, 1, 0, 0, 1],
+#    [1, 0, 0, 0, 0],
+#    [0, 1, 0, 0, 0]
+#]
+
+nodes : dict = {}
+connections : dict = {}
+
+num_connections = 0
+
+steps : int
+
+log_num_nodes : int
+log_num_connections : int
+total_num_qubits : int
+
+CXGate : XGate
+
+application_list_CXGate : list
+application_list_coin : list
+application_list_shift : list
+
+gates : list = [] # Stored gates so when running big circuits it avoids wasteful repeating of gate calculations
+
+qc : QuantumCircuit
+
+def gatherData() -> None:
+
+    # Telling python to update the global variables instead of creating local ones
+    global nodes
+    global connections
+
+    global num_connections
     
-    return degree
-
-def isConnected(node_index, connection_id):
-
-    for i in range(0, mat_size):
-        if matrix_ids[node_index][i] == connection_id:
-            return adj_matrix[node_index][i]
-    return 0
-
-def getAmplitude(node_index):
-
-    degree = getDegree(node_index)
-    amplitudes = []
-    for i in range(0, num_connections): # Going through all the possibilities
-        if isConnected(node_index, i):
-            amplitudes.append(1/np.sqrt(degree))
-        else:
-            amplitudes.append(0)
-
-    return amplitudes
-        
-def addCoin(node_index):
-
-    binary = bin(node_index)[2:].zfill(log_size) # Getting the binary representation of the numbers with the correct amount of bits because python cuts them for some reason
-
-    sub_qc = QuantumCircuit(log_num_connections)
-
-    amplitudes = getAmplitude(node_index)
-
-    # Apply Si dagger
-    sub_qc.prepare_state(amplitudes)
-
-    for i in range(0, log_num_connections):
-        sub_qc.x(i)
-
-    sub_qc.h(log_num_connections-1)
-    sub_qc.append(CXGate, sub_control_list_coin)
-    sub_qc.h(log_num_connections-1)
-
-    for i in range(0, log_num_connections):
-        sub_qc.x(i)
-
-    # Apply Si
-    sub_qc.prepare_state(amplitudes).inverse()
-
-    coin_gate = sub_qc.to_gate().control(log_size, label=f"C{node_index}", ctrl_state=binary[::-1]) # Reversing the binary to make it identical to the research paper
-
-    qc.append(coin_gate, control_list_coin)
-
-def addShift(connection_id):
-
-    binary = bin(connection_id)[2:].zfill(log_num_connections)
-
-    sub_qc = QuantumCircuit(log_size)
-
-    found = 0
-
-    for i in range(0, mat_size):
-        if found:
-            break
-        for j in range(0, mat_size):
-            if matrix_ids[i][j] == connection_id:
-                row = i
-                column = j
-                found = 1
-                break
-
-    binary_row = bin(row)[2:].zfill(log_size)
-    binary_column = bin(column)[2:].zfill(log_size)
-
-    for i in range(0, log_size):
-        if binary_column[i] != binary_row[i]:
-            sub_qc.x(i)
-
-    shift_gate = sub_qc.to_gate().control(log_num_connections, label=f"S{connection_id}", ctrl_state=binary[::-1])
-
-    qc.append(shift_gate, control_list_shift) # Inverted so the controls are on the bottom
-
-def prepare():
-
-    amplitudes = []
-
-    for i in range(0, mat_size * num_connections):
-            amplitudes.append(0)
-
-    for node in range(0, mat_size):
-        for connection in range(0, num_connections):
-            if (isConnected(node, connection)):
-                
-                node_binary = bin(node)[2:].zfill(log_size)
-                connection_binary = bin(connection)[2:].zfill(log_num_connections)
-                full_binary = node_binary + connection_binary
-
-                print(node_binary + " " + connection_binary)
-                index = int(full_binary, 2)
-                amplitudes[index] = 1/np.sqrt(getDegree(node))
-
-
-    sqrt_n = np.sqrt(mat_size)
-
-    print(amplitudes)
-
-    for i in range(0, mat_size * num_connections):
-        amplitudes[i] *= 1/sqrt_n
-
-    print("\n\n")
-    print(amplitudes)
-    qc.prepare_state(amplitudes)
-
-if __name__ == "__main__":
-
-    # ===================================
-    # Input
-    # ===================================
-
-    mat_size = int(input("Adjacency matrix's order: "))
-
-    log_size = int(np.ceil(np.log2(mat_size))) # Will be used to get the binary representation of a number
-
-    num_connections = 0
-
-    adj_matrix = []
-
-    matrix_ids = []
+    global steps
     
-    #adj_matrix = [
-    #    [0, 1, 1, 1],
-    #    [1, 0, 1, 0],
-    #    [1, 1, 0, 0],
-    #    [1, 0, 0, 0]
-    #]
-#
-    #matrix_ids = [
-    #    [0, 1, 1, 1],
-    #    [1, 0, 1, 0],
-    #    [1, 1, 0, 0],
-    #    [1, 0, 0, 0]
-    #]
+    global log_num_nodes
+    global log_num_connections
+    global total_num_qubits
 
-    adj_matrix = [
-    [0, 0, 1, 0, 0, 0, 0, 1],  # vértice 0
-    [0, 0, 0, 0, 1, 0, 0, 0],  # vértice 1
-    [1, 0, 0, 0, 1, 0, 1, 0],  # vértice 2
-    [0, 0, 0, 0, 1, 1, 0, 0],  # vértice 3
-    [0, 1, 1, 1, 0, 0, 0, 0],  # vértice 4
-    [0, 0, 0, 1, 0, 0, 0, 0],  # vértice 5
-    [0, 0, 1, 0, 0, 0, 0, 1],  # vértice 6
-    [1, 0, 0, 0, 0, 0, 1, 0]   # vértice 7
-    ]
+    global CXGate
 
-    matrix_ids = [
-    [0, 0, 1, 0, 0, 0, 0, 1],  # vértice 0
-    [0, 0, 0, 0, 1, 0, 0, 0],  # vértice 1
-    [1, 0, 0, 0, 1, 0, 1, 0],  # vértice 2
-    [0, 0, 0, 0, 1, 1, 0, 0],  # vértice 3
-    [0, 1, 1, 1, 0, 0, 0, 0],  # vértice 4
-    [0, 0, 0, 1, 0, 0, 0, 0],  # vértice 5
-    [0, 0, 1, 0, 0, 0, 0, 1],  # vértice 6
-    [1, 0, 0, 0, 0, 0, 1, 0]   # vértice 7
-    ]
-    # Reading the adjacency matrix
-    #for i in range(0, mat_size):
-    #    row_str = input(f"row {i+1} (space separated): ")
-    #    row_elements = list(map(int, row_str.split())) # This is horrible to read but works flawlessly
-    #    adj_matrix.append(row_elements)
-    #    matrix_ids.append(row_elements)
+    global application_list_CXGate
+    global application_list_coin
+    global application_list_shift
 
-    adj_matrix = np.array(adj_matrix)
+    for i in range(num_nodes):
+        nodes[i] = {"degree": 0, "connections": []}
 
-    matrix_ids = np.array(matrix_ids)
-    
-    # Giving the ids
-    for i in range(0, mat_size):
-        for j in range(0, mat_size):
-            matrix_ids[i][j] = -1
-    
-    for i in range(0, mat_size):
-        for j in range(0, mat_size):
-            if adj_matrix[i][j] == 1 and i < j:
-                matrix_ids[i][j] = num_connections
-                matrix_ids[j][i] = num_connections
+    for i in range(num_nodes):
+        for j in range(i, num_nodes):
+            if (adj_matrix[i][j]):
+                connections[num_connections] = {"node1": i, "node2": j}
+                nodes[i]["degree"] += 1
+                nodes[i]["connections"].append(num_connections)
+                nodes[j]["degree"] += 1
+                nodes[j]["connections"].append(num_connections)
                 num_connections += 1
+
+    steps = 1#int(input("How many steps should the walk simulate?: "))
+
+    log_num_nodes = int(np.ceil(np.log2(num_nodes)))
 
     log_num_connections = int(np.ceil(np.log2(num_connections)))
 
-    # ===================================
-    # Creating the Circuit and running it
-    # ===================================
-
-    qc = QuantumCircuit(log_size + log_num_connections, log_size)
-
-    prepare()
-
-    control_list_coin = range(log_size + log_num_connections)
-    sub_control_list_coin = range(log_num_connections) # List of the controls to the CX gate inside the coin
-
-    control_list_shift = []
-    print(matrix_ids)
-    # Doing all this so the first items of the list are the connections's qubits (control) and the other ones are the node's qubits (affected)
-    for i in range(0, log_num_connections):
-        control_list_shift.append(i + log_size)
-    for i in range(0, log_size):
-        control_list_shift.append(i)
+    total_num_qubits = log_num_nodes + log_num_connections
 
     CXGate = XGate().control(log_num_connections-1)
+    application_list_CXGate = list(range(log_num_connections))
 
-    # for i in range(0, 1):
-    #     addCoin(i)
+    application_list_coin = list(range(log_num_connections, total_num_qubits)) + list(range(log_num_connections))
 
-    # for i in range(0, num_connections):
-    #     addShift(i)
+    application_list_shift = list(range(total_num_qubits))
 
-    #qc.measure(range(log_size), range(log_size))
+def prepareCircuit() -> None:
+
+    amplitudes = []
+
+    num_possibilities = 2 ** (total_num_qubits)
+
+    for i in range(num_possibilities):
+        amplitudes.append(0)
+
+    for node in range(num_nodes):
+
+        inverse_of_sqrt_degree = 1/np.sqrt(nodes[node]["degree"])
+
+        for connection in nodes[node]["connections"]:
+            binary_node = bin(node)[2:].zfill(log_num_nodes)
+            binary_connection = bin(connection)[2:].zfill(log_num_connections)
+            state = binary_node + binary_connection
+            amplitudes[int(state, 2)] = inverse_of_sqrt_degree
+
+    inverse_of_sqrt_num_nodes = 1/np.sqrt(num_nodes)
+
+    for i in range(num_possibilities):
+        if amplitudes[i] != 0: # mathematically doesn't make a difference but is prittier to print and skip some divisions and multiplications
+            amplitudes[i] *= inverse_of_sqrt_num_nodes
+
+    qc.prepare_state(amplitudes)
+
+def getAmplitudeOfNode(node : int) -> list:
+
+    amplitudes = []
+
+    inverse_of_sqrt_degree = 1/np.sqrt(nodes[node]["degree"])
+
+    for i in range(2 ** log_num_connections):
+        amplitudes.append(0)
+
+    for connection in nodes[node]["connections"]:
+        amplitudes[connection] = inverse_of_sqrt_degree
+
+    return amplitudes
+
+def addCoin(node: int) -> None:
+
+    binary_node = bin(node)[2:].zfill(log_num_nodes)
+
+    sub_qc_coin = QuantumCircuit(log_num_connections)
+
+    amplitudes = getAmplitudeOfNode(node)
+
+    # Apply Si dagger
+    sub_qc_coin.prepare_state(amplitudes).inverse()
+
+    for i in range(0, log_num_connections):
+        sub_qc_coin.x(i)
+
+    sub_qc_coin.h(0)
+    sub_qc_coin.append(CXGate, application_list_CXGate[::-1])
+    sub_qc_coin.h(0)
+
+    for i in range(0, log_num_connections):
+        sub_qc_coin.x(i)
+
+    # Apply Si
+    sub_qc_coin.prepare_state(amplitudes)
+
+    coin_gate = sub_qc_coin.to_gate().control(log_num_nodes, label=f"C{node}", ctrl_state=binary_node)
+
+    gates.append([coin_gate, application_list_coin])
+
+    qc.append(coin_gate, application_list_coin)
+
+def addShift(connection : int) -> None:
+
+    binary_connection = bin(connection)[2:].zfill(log_num_connections)
+
+    sub_qc_shift = QuantumCircuit(log_num_nodes)
+
+    binary_node1 = bin(connections[connection]["node1"])[2:].zfill(log_num_nodes)
+    binary_node2 = bin(connections[connection]["node2"])[2:].zfill(log_num_nodes)
+
+    for qubit in range(log_num_nodes):
+        if binary_node1[qubit] != binary_node2[qubit]:
+            # Applying the X gates from bottom to top, so that 100 is not applied as 001 and such, basically Qiskit ordering being dumb
+            sub_qc_shift.x(log_num_nodes-1 - qubit)
+
+    shift_gate = sub_qc_shift.to_gate().control(log_num_connections, label=f"S{connection}", ctrl_state=binary_connection)
+
+    gates.append([shift_gate, application_list_shift])
+    qc.append(shift_gate, application_list_shift)
+
+def getProbabilities() -> None:
+
+    sv = Statevector(qc)
+    prob_dict = sv.probabilities_dict()
+
+    probabilities = []
+
+    for node in range(num_nodes):
+
+        prob = 0
+
+        for connection in range(num_connections):
+
+            binary_node = bin(node)[2:].zfill(log_num_nodes)
+            binary_connection = bin(connection)[2:].zfill(log_num_connections)
+
+            full_binary = binary_node + binary_connection
+
+            prob += prob_dict[full_binary] if full_binary in prob_dict else 0
+
+        probabilities.append(prob)
+
+    plt.bar(range(num_nodes), probabilities)
+    plt.show()
+
+if __name__ == "__main__":
+
+    gatherData()
+
+    qr_nodes = QuantumRegister(log_num_nodes, 'q')
+    qr_connections = QuantumRegister(log_num_connections, 'l')
+    cr = ClassicalRegister(log_num_nodes, 'c')
+
+    qc = QuantumCircuit(qr_connections, qr_nodes, cr)
+
+    prepareCircuit()
+
+    for node in range(num_nodes):
+        addCoin(node)
+
+    for connection in range(num_connections):
+        addShift(connection)
+
+    for step in range(1, steps):
+        for gate in range(len(gates)):
+            qc.append(gates[gate][0], gates[gate][1]) # Appending the gate (gate[gate][0]) to it's application list (gates[gate][1])
+
+    getProbabilities()
 
     qc.draw(output="mpl")
     plt.show()
-
-    #backend = StatevectorSimulator()
-
-    #transpiled_circuit = transpile(qc, backend)
-    #transpiled_circuit.draw('mpl')
-    #plt.show()
-    #job = backend.run(backend)
-    #print(job.result())
-    #counts = job.result().get_counts()
-    #plt.bar(list(counts.keys()), list(counts.values()))
-    #plt.show()
-    #print(counts)
-    statevector = Statevector(qc)
-    print(statevector)
-    
-    probs = []
-    
-    for node in range(0, mat_size):
-        prob = 0
-        for connection in range(0, num_connections):
-            #if (isConnected(node, connection)):
-                
-                node_binary = bin(node)[2:].zfill(log_size)
-                connection_binary = bin(connection)[2:].zfill(log_num_connections)
-                full_binary = node_binary + connection_binary
-                index = int(full_binary, 2)
-                print(node_binary + " " + connection_binary + " = " + str(abs(statevector[index]) ** 2))
-                
-                #amplitudes[index] = 1/np.sqrt(getDegree(node))
-                prob += abs(statevector[index]) ** 2
-        probs.append(prob)
-
-    plt.bar(list(range(mat_size)), probs)
-    plt.show()
-                
